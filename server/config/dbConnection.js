80eeb484a9260b73ed429938391909b50f258f10
Pedro Galindo <pedrogalindo0911@gmail.com>
2024-08-31 10:37:39 -0600

    feat: Feat Connection Db
    In this commit we added the connection to the database

---
+++ server/config/dbConnection.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false, 
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Conexión a la base de datos exitosa'))
+  .catch(err => console.error('No se pudo conectar a la base de datos:', err));
+
+module.exports = sequelize;
--- server/index.js
+++ server/index.js
@@ -20,19 +20,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
-dbConnection.js: const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
-  logging: false, 
-});
-
-sequelize.authenticate()
-  .then(() => console.log('Conexión a la base de datos exitosa'))
-  .catch(err => console.error('No se pudo conectar a la base de datos:', err));
-
-module.exports = sequelize;
